7648ef2fc610f2acf141d73ea57d7b7411b9480c
k5tis5jiloma <JD08RK@student.aau.dk>
2021-09-08 16:55:29 +0200

    Et program der beregner hypotenusen

--- første_kode.c
+++ første_kode.c
@@ -1,11 +1,26 @@
-//Opgave 11 side 125
+/*Opgave 11 side 125*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    double m, n, side_1;
-    side_1 = (m*m)-(n*n);
+    int m, n, side_1, side_2, hypotenuse;
+    
+    printf("Skriv m > ");
+    scanf(" %d", &m);
+    printf("\n Skriv n >");
+    scanf(" %d", &n);
 
-    printf("Heeelo");
+    if (m>n)
+    {
+       side_1 = (m*m)-(n*n);
+       side_2 = 2*m*n;
+       hypotenuse = (m*m) + (n*n);
+
+       printf("\nSide 1 er: %d  Side 2 er: %d  Hypetenusen er: %d", side_1, side_2, hypotenuse);
+    }
+    else
+        printf("M skal vaere stoerre end N!");
     return 0;
 }
