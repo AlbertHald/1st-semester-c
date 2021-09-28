fb6d630507081bae254c253569eae4fb3ac1a033
Hald Stationær <JD08RK@student.aau.dk>
2021-09-28 18:26:38 +0200

    Resten af aflevering 3!

--- Afleveringer/afl-3-divisor.c
+++ Afleveringer/afl-3-divisor.c
@@ -1,39 +1,51 @@
+/* Et program der finder største fælles divisor */
+/* 27-09-2021 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 int main(void)
 {
-    int input_1, input_2, modulus_out, i, GCD = 0, num_found = 0; 
-    /* Skriv to positive hele tal */
-    scanf("%d", &input_1);
-    scanf("%d", &input_2);
-    /* Har to tal. finder det største og tager divisoren af det */
-    int max_input = (input_1 >= input_2) ? input_1 : input_2; 
-    int min_input = (input_1 < input_2) ? input_1 : input_2;
-    /* Så længe number ikke er found kører den videre */
-    /* Hvor i er fælles divisor */
-    /* Kører igennem indtil den største divisor er fundet */
-    if (max_)
-    {
-        /* code */
-    }
-    
-    i = min_input;
-    while (!num_found)
-    {
-        if (max_input % i == 0 && min_input % i == 0)
+    int input_1, input_2, pos_divisor; 
+    char GoAgain;
+    do
+    {    
+        int GCD = 0,  NumFound = 0;
+        /* Skriv to positive hele tal */
+        printf("Skriv et helt positivt tal > ");
+        scanf(" %d", &input_1);
+        printf("Skriv et helt positivt tal mere > ");
+        scanf(" %d", &input_2);
+        /* Finder det største tal af de to inputs */
+        int max_input = (input_1 >= input_2) ? input_1 : input_2; 
+        int min_input = (input_1 < input_2) ? input_1 : input_2;
+        
+        /* Tjekker om et af de indtastet tal er 0 */
+        if (min_input == 0)
         {
-            num_found = 1;
+            pos_divisor = max_input;
+            NumFound = 1;
         }
-            
-        else
+        
+        /* Kører fra det laveste af de to tal og ned til 0.
+        Stopper ved den første (største) divisor for begge */
+        pos_divisor = min_input;
+        while (!NumFound)
         {
-            i--;
+            if (max_input % pos_divisor == 0 && min_input % pos_divisor == 0)
+            {
+                NumFound = 1;
+            }    
+            else
+            {
+                pos_divisor--;
+            }         
         }
-            
+        printf("GCD for de to tal %d og %d er derfor: %d \n", max_input, min_input, pos_divisor); 
+        /* Skal programmet køres igen? */
+        printf("Skal programmet koere igen? Tryk Y/y ellers tryk en vilkaarlig knap");
+        scanf(" %c", &GoAgain);
     }
-    printf("GCD er derfor %d", i);
-    
+    while (GoAgain == 'y' || GoAgain == 'Y');
     return 0;
 }
