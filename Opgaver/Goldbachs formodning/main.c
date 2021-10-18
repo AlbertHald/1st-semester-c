fae1c4cd887b9efce31aa0b9752c98a512ed22de
Hald Stationær <JD08RK@student.aau.dk>
2021-10-18 16:14:04 +0200

    Hallo der er ændringer

---
+++ Opgaver/Goldbachs formodning/main.c
@@ -0,0 +1,12 @@
+/* Alle lige heltal større end 6 kan udtrykkes som summen af to ulige primtal */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void)
+{
+    printf("Alle lige heltal større end 6 kan udtrykkes som summen af to ulige primtal. Tjekker i intervallet 7 til 2.000.000!");
+    
+    return 0;
+}
---
+++ Opgaver/Goldbachs formodning/primes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+/* Laver min egen headerfil */
+#include "primes.h"
+
+/* Return if i is a prime number. 
+   It is assumed as a precondition that the parameter i is non-negative */
+int isPrime(int i){
+   assert(i >= 0);
+
+   if (i == 1) 
+     return 0;
+   else if (i == 2) 
+     return 1;
+   else if (i % 2 == 0)
+     return 0;
+   else{
+     int k, limit;
+     limit = (int)(ceil(sqrt(i)));
+     for (k = 3; k <= limit; k += 2)
+        if (i % k == 0)
+           return 0;
+     return 1;
+   }
+}
