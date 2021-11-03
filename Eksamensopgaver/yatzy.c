92e551dad8daf79866a05e5124c3237667c4543e
Hald Stationær <JD08RK@student.aau.dk>
2021-11-03 23:15:47 +0100

    Ny yatzy

--- Eksamensopgaver/yatzy.c
+++ Eksamensopgaver/yatzy.c
@@ -8,6 +8,7 @@
 
 int amountOfDice();
 int playYatzy(int*, int);
+void playUpperPart(int dice_array[], int dice_ammount, int score[]);
 int rollDice(int *dice_array, int dice_ammount);
 
 int main(void)
@@ -33,30 +34,36 @@ int amountOfDice(){
         printf("How many dice do you want to play with?\n\nYou must play with atleast 5 dice >");
         scanf(" %d", &dice);
         
-        //Flush the input if its a char. 
+        //Flush the input if its a char. Lav en function Albert Hald !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         while ((getchar()) != '\n');
     }
 
-    printf("\nHjælp mig kurt");
     return dice;
 }
 
-int playYatzy(int *score, int dice_ammount)
+int playYatzy(int score[], int dice_ammount)
 {
     int *dice_array = (int*)malloc(dice_ammount * sizeof(int));
     //Plays upperpart, shall return the score to the elements in the "score" array
-    void playUpperPart();
+    playUpperPart(dice_array, dice_ammount, score);
     //Plays lowerpart
-
+    for (int i = 0; i < dice_ammount; i++)
+    {
+        printf(" %d ", dice_array[i]);
+    }
+    
     //Give a final score
     free(dice_array); 
 }
 
+void playUpperPart(int dice_array[], int dice_ammount, int score[]){
+    rollDice(dice_array, dice_ammount);
+}
+
 //Should return the array but with randomized numbers
-int rollDice(int *dice_array, int dice_ammount){
+int rollDice(int dice_array[], int dice_ammount){
     for (int current_dice = 0; current_dice < dice_ammount; current_dice++)
     {
-        *dice_array[current_dice] = (rand() % 5) + 5; 
-    }
-    
+        dice_array[current_dice] = (rand() % 6) + 1; 
+    } 
 }
