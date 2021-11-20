37442e8ce24d6593513aa3f8b0bffff5e6349b85
AlbertHald <albert24122@gmail.com>
2021-11-20 14:16:27 +0100

    Started division

---
+++ Opgaver/Rekursion/broeker_struct.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Fractions
+{
+    int numerator;
+    int denuminator;
+};
+typedef struct Fractions Fractions;
+
+
+void shortenFraction(Fractions);
+produceFraction();
+multiplyFractionAndNum();
+multiplyFractions();
+addFractions();
+
+int main(void)
+{
+    Fractions frac;
+
+    printf("Input numerator > ");
+    scanf(" %d", &frac.numerator);
+    printf("\nInput denuminator > ");
+    scanf(" %d", &frac.denuminator);
+
+    shortenFraction(frac);
+    return EXIT_SUCCESS;
+}
+
+void shortenFraction(Fractions frac){
+    
+}
+
---
+++ Opgaver/Rekursion/rekursiv_divi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    int dividend, divisor;
+    printf("Type dividend > ");
+    scanf(" %d", &dividend);
+    printf("\nType divisor >");
+    scanf(" %d", &divisor);
+
+    int division_answer = division(dividend, divisor);
+
+    return 0;
+}
+
+
+int division(int divident, int divisor) {
+    if (/* condition */)
+    {
+        
+    }
+    
+}
+//Function that recursivly divides by using the "-" operator 
